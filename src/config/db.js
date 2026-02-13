f2296dbae430d4b5c3d502f84cd335dad692c542
MohitMaurya63 <mohitmaurya644@gmail.com>
2026-02-13 17:48:30 +0530

    latest changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,4 @@
-// const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//   await mongoose.connect(process.env.MONGO_URI);
-//   console.log("MongoDB Connected");
-// };
-
-// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
--- src/server.js
+++ src/server.js
@@ -1,24 +1,4 @@
-// const http = require("http");
-// const connectDB = require("./config/db");
-// require("dotenv").config();
 
-// const app = require("./app");
-// const server = http.createServer(app);
-
-// connectDB();
-
-// const io = require("socket.io")(server, {
-//   cors: { origin: "*" },
-// });
-
-// const socketInstance = require("./socketInstance");
-// socketInstance.setIO(io); // 🔥 SET ONCE
-
-// require("./socket")(io);
-
-// server.listen(5000, () => {
-//   console.log("Backend running on port 5000");
-// });
 
 const http = require("http");
 const connectDB = require("./config/db");
@@ -50,38 +30,3 @@ server.listen(PORT, () => {
 });
 
 
-// const http = require("http");
-// const connectDB = require("./config/db");
-// require("dotenv").config();
-
-// const app = require("./app");
-// const server = http.createServer(app);
-
-// connectDB();
-
-// /* ================= CORS ORIGINS ================= */
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   // "https://jts-vm.netlify.app",
-//   "https://jts-vms.vercel.app"   // ✅ ADD THIS
-
-// ];
-
-// /* ================= SOCKET.IO ================= */
-// const io = require("socket.io")(server, {
-//   cors: {
-//     origin: allowedOrigins,
-//     credentials: true,
-//   },
-// });
-
-// const socketInstance = require("./socketInstance");
-// socketInstance.setIO(io);
-
-// require("./socket")(io);
-
-// const PORT = process.env.PORT || 5000;
-
-// server.listen(PORT, () => {
-//   console.log(`Backend running on port ${PORT}`);
-// });
